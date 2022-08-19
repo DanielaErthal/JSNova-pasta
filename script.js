b56d93416bd64a4214ac53d31a85142f1c5e0b1c
DaniErthal <erthaldaniela17@gmail.com>
2022-08-19 16:58:31 -0300

    meus js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 alert("Hello wellcome")
+
+var nomes = ["Igor","Jose","Maria"];
+var A = [7.0,6.5,9.5];
+var B = [8.0,7.0,8.5];
+
+function media(n1, n2) {
+    return(n1+n2)/2;
+}
+
+function passou(media) {
+    if (media>7){
+        return "Aprovado";
+    }
+     else{
+        return "Rprovado";
+         }       
+}
+
+    
